26c1339b3ff1c824e1ae09123bf60896788922be
fidyras <65243388+fidyras@users.noreply.github.com>
2021-11-03 09:05:53 +0300

    placeholder for report and dashboard

--- code/day1_3_dplyr_intro.R
+++ code/day1_3_dplyr_intro.R
@@ -2,10 +2,12 @@ library(ggplot2)
 library(dplyr)
 library(sf)
 #importer la base de donnees qui contient les cas de la maladie X
-data<-read.csv("course MoH/data/dataset1.csv")
+data<-read.csv("code/dataset1.csv")
 table(as.factor(data$sexe))
 
 #####dplyr basics
+
+
 ## filtrer les donnees: exemple on ne veut examiner que les donnees des cas de sexe masculin
 
 #la fonction filter() permet de voir les donnees en fonction de certaines valeurs
@@ -17,4 +19,41 @@ filter(data, region=="Analamanga",age<2)
 #dplyr ne modifie pas la base donnee initiale, mais on peut creer un nouvel objet en utilisant <-
 enfants_AN<-filter(data, region=="Analamanga",age<2)
 
+# trier les donnees
+## trier les cas de la region Melaky, et uniquement la region Melaky en fonction de l'age des cas
 
+MK<-filter(data, region=="Melaky")
+MKage<-arrange(MK,age)
+
+#piping with dplyr "%>%"
+
+MKage2<-data%>%
+  filter(region=="Melaky")%>%
+  arrange(age)
+
+
+## Summary statistics
+
+# count number of cases
+data%>%
+  summarise(cas=n())
+
+# count number of cases per region
+data%>%
+  group_by(region)%>%
+  summarise(cas=n())
+
+
+
+# count number of cases per region per day
+data%>%
+  group_by(region,date)%>%
+  summarise(cas=n())
+
+
+## ajouter une nouvelle colonne (mutate)
+
+data%>%
+  group_by(region,date)%>%
+  summarise(cas=n())%>%
+  mutate(rollmean=rollmeanr(cas,7,fill=NA))->moyenne_region
